adf1ceb2f62dadc7297cf0c3978e01f40f264560
CRAlex254 <alexgichuru13703@gmail.com>
2023-03-17 15:42:17 -0700

    initial commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("Size of char in c= %2d bytes \n", size of (char));
+	printf("Size of int in c= %2d bytes \n", size of (int));
+	printf("Size of long int in c= %2d bytes \n", size of (long int));
+	printf("Size of long long int in c= %2d bytes \n", size of (long long int));
+	printf("Size of float in c= %2d bytes \n", size of (float));
+	return(0);
+}	
