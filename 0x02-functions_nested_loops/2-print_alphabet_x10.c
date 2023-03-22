0d778c5168b464d85b48eb04d2344c1678832312
CRAlex254 <alexgichuru13703@gmail.com>
2023-03-22 12:31:04 -0700

    initial commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@
 void print_alphabet_x10(void)
 {
 	char i;
-	int count = 0;
+	int count = 1;
 
-	while (count++ <= 10) 
+	while (count++ <= 10)
 	{
 		for (i = 'a' ; i <= 'z' ; i++)
 			_putchar(i);
