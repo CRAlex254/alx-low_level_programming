b68ce1adfae22ac17c8388faa5f19045eb4d55d4
CRAlex254 <alexgichuru13703@gmail.com>
2023-03-22 13:56:56 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower(int c): function that checks for lowercase character
+ *
+ * Return: 1
+ * Return: o if otherwise
+ */
+
+int _islower(int c);
+{
+	int c;
+
+	if (c is lowercase)
+		return = 1;
+	else 
+		return = 0;
+}
