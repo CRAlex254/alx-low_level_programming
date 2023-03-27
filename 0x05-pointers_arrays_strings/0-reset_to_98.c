35864cb91402cef088ea601c5737bbc0e5c8a327
CRAlex254 <alexgichuru1703@gmail.com>
2023-03-27 16:55:52 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that resets the value of n to 98
+ * @n: parameter to be changed
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
