675ad1ceb28a3508a36515b3cb1d4b7e5644b02b
CRAlex254 <alexgichuru1703@gmail.com>
2023-03-27 17:39:37 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns lenghs of a string.
+ * @s: string whose lengh is to be printed
+ * Return: lengh of string or the count
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = '0' ; s[i] != '\0' ; i++)
+	{
+		count++;
+	}
+	return (count);
+}
