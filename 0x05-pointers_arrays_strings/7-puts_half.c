a246b5031f5d0ca2de7b2d7a5d6ecf765c4ab84c
CRAlex254 <alexgichuru1703@gmail.com>
2023-03-28 14:47:36 -0700

    intial commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half the string
+ * then followed by a new line.
+ * @str: string that is to be printed.
+ */
+
+void puts_half(char *str)
+{
+	int a, n, c;
+
+	a = 0;
+
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+
+	if (a % 2 == 0)
+	{
+		for (c = a / 2; str[c] != '\0'; c++)
+		{
+			_putchar(str[c]);
+		}
+	}
+	else if (a % 2)
+	{
+		for (n = (a - 1) / 2; n < a - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
+	}
+	_putchar('\n');
+}
